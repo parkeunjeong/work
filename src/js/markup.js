82e7627272f47d449ffb3f955c9b3fb0573f979c
parkeunjeong <sambaamigo@naver.com>
2014-12-10 09:25:23 +0900

    js 추가

---
+++ src/js/markup.js
@@ -0,0 +1,15 @@
+(function(){
+
+	var htCurrency  = {
+		'KRW' : '원',
+		'USD' : '달러',
+		'EUR' : '유로',
+		'JPY' : '엔',
+		'CNY' : '위안',
+		'CAD' : '달러',
+		'AUD' : '달러',
+		'NZD' : '달러'
+	};
+
+//			var oExchange =  new nhn.mobile.search.Exchange($$.getSingle("div.cs_exchangerate_cal"), htCurrency);
+});
